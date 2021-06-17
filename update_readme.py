4d9fd3a53e993b44c77a76c988340ae036f926a5
Kevin Hsu <kevin860612@gmail.com>
2021-06-18 03:09:41 +0800

    Update update_readme.py

--- update_readme.py
+++ update_readme.py
@@ -9,6 +9,7 @@ TOKEN = os.environ.get("GH_TOKEN", "")
 early_brid_url = "https://gist.githubusercontent.com/kevin12686/e81c734140d5e860feb3aec007be3177/raw"
 code_diff_url = "https://gist.githubusercontent.com/kevin12686/6a87c1d1afda559d3eb404ba325b3dc7/raw/"
 
+
 def replace_chunk(content, marker, chunk, inline=False):
     r = re.compile(
         rf"<!\-\- {marker} start \-\->.*<!\-\- {marker} end \-\->",
@@ -19,18 +20,19 @@ def replace_chunk(content, marker, chunk, inline=False):
     chunk = f"<!-- {marker} start -->{chunk}<!-- {marker} end -->"
     return r.sub(chunk, content)
 
+
 def httpx_get(url):
     return httpx.get(url)
 
 
 if __name__ == "__main__":
     readme = root / "readme.md"
-    readme_contents = readme.open().read()
+    readme_contents = readme.open(encoding="utf8").read()
 
     early_brid_text = f"\n```text\n{httpx_get(early_brid_url).text}\n```\n"
-    readme_contents = replace_chunk(readme_contents, "early_brid", early_brid_text)
+    readme_contents = replace_chunk(readme_contents, "early_bird", early_brid_text)
 
     code_diff_text = f"\n```text\n{httpx_get(code_diff_url).text}\n```\n"
     readme_contents = replace_chunk(readme_contents, "code_diff", code_diff_text)
 
-    readme.open("w").write(readme_contents)
+    readme.open("w", encoding="utf8").write(readme_contents)
