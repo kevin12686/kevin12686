7487a58856f5ccb29900be3edcf0d5c490835c04
Kevin Hsu <kevin860612@gmail.com>
2022-07-20 17:27:53 -0500

    Update update_readme.py

--- update_readme.py
+++ update_readme.py
@@ -6,6 +6,7 @@ import os
 root = pathlib.Path(__file__).parent.resolve()
 TOKEN = os.environ.get("GH_TOKEN", "")
 
+spotify_url = "https://gist.githubusercontent.com/kevin12686/091278d57232ae0ac0fbbc74b6a8cbbb/raw/"
 early_brid_url = "https://gist.githubusercontent.com/kevin12686/e81c734140d5e860feb3aec007be3177/raw"
 code_time_url = "https://gist.githubusercontent.com/kevin12686/0b22d1886d3af2e8e235b3c46e768064/raw/"
 codestats_url = "https://gist.githubusercontent.com/kevin12686/98d3939c7c75faeaceec1881dc77c16d/raw/"
@@ -30,6 +31,9 @@ if __name__ == "__main__":
     readme = root / "readme.md"
     readme_contents = readme.open(encoding="utf8").read()
 
+    spotify_text = f"\n```text\n{httpx_get(spotify_url).text}\n```\n"
+    readme_contents = replace_chunk(readme_contents, "spotify", spotify_text)
+    
     early_brid_text = f"\n```text\n{httpx_get(early_brid_url).text}\n```\n"
     readme_contents = replace_chunk(readme_contents, "early_bird", early_brid_text)
     
